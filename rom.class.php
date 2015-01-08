bf3ea1c4e96f04b46d288fefd2873dece5b76599
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 20:30:24 +0100

    Bump version to 1.0.0

--- rom.class.php
+++ rom.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('rom')) {
 	class rom extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.1';
+		public $version				= '1.0.0';
 		protected $this_game		= 'rom';
 		protected $types			= array('classes', 'races', 'factions', 'filters');
 		protected $classes			= array();
